ce46ecac2a3e733406d72eb149e7e3e7a3a93de2
Jakub2472001 <jakub.Dobrowolski@pl.ey.com>
2025-07-03 09:14:15 +0000

    Added error handling to prevent task failure and log error messages and handle duplicate offer data inserts - PSE

--- dags/pse_etl_main_expand.py
+++ dags/pse_etl_main_expand.py
@@ -6,12 +6,11 @@ from io import StringIO
 import pandas as pd
 import os
 import sys
+import logging
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from tasks.pse_dag_config import POSTGRES_CONN_ID, HTTP_CONN_ID, HISTORICAL_START_DATE, ENTITY_CONFIG
-
-import logging
 
 logger = logging.getLogger(__name__)
 
@@ -21,7 +20,7 @@ def create_entity_tables(task_param: dict):
     entity = task_param["entity"]
     columns = task_param["columns"]
     cols_sql = ",\n".join([f'"{col}" TEXT' for col in columns])
-    unique_cols = None if entity in ["poeb-rbn", "poeb-rbb", "pomb-rbn"] else '"business_date", "dtime"'
+    unique_cols = None if entity in ["poeb-rbn", "poeb-rbb", "pomb-rbn", "popmb-rmb"] else '"business_date", "dtime"'
 
     staging_sql = f"""
     CREATE TABLE IF NOT EXISTS airflow_data."{task_param['staging_table']}" (
@@ -118,11 +117,12 @@ def fetch_entity_data(task_param: dict, execution_date=None) -> dict:
         return {
             "entity": entity,
             "columns": columns,
-            "df": df.to_dict(orient="records")
+            "df": df.to_dict(orient="records"),
+            "error": None,
         }
     except Exception as e:
         error_message = str(e)
-        logger.error(f"Error fetching data for {entity} on {date_str}: {error_message}")
+        logger.exception(f"Error fetching data for {entity} on {date_str}: {error_message}")
 
          # Jeśli już był fail, aktualizujemy message
         if log_result and result_status == "fail":
@@ -142,118 +142,204 @@ def fetch_entity_data(task_param: dict, execution_date=None) -> dict:
 
 @task
 def load_to_staging(data: dict):
+    entity = data.get("entity")
+    fetch_error = data.get("error", "Empty DataFrame loaded")
+    logger.warning(f"[STAGING] 1 load_to_staging for {entity} – reason: {fetch_error}")
+
     if data.get("skipped"):
         return {
+            "entity": entity,
             "staging_table": None,
-            "entity": data["entity"],
             "skipped": True,
-            "error": data.get("skip_reason")
+            "error": data.get("skip_reason", "Skipped upstream")
         }
 
-    entity = data["entity"]
-    columns = data["columns"]
-    df = pd.DataFrame(data["df"])
+    try:
+        columns = data["columns"]
+        df = pd.DataFrame(data["df"])
+        if df.empty:
+            return {
+                "entity": entity,
+                "staging_table": None,
+                "skipped": True,
+                "error": fetch_error
+            }
 
-    if df.empty:
-        return {"staging_table": None, "entity": entity, "skipped": True, "error": data.get("error")}
+        staging_table = ENTITY_CONFIG[entity]["staging_table"]
+        df = df[columns]
 
-    staging_table = [v["staging_table"] for k, v in ENTITY_CONFIG.items() if k == entity][0]
-    df = df[columns]
+        buffer = StringIO()
+        df.to_csv(buffer, index=False)
+        buffer.seek(0)
 
-    buffer = StringIO()
-    df.to_csv(buffer, index=False)
-    buffer.seek(0)
+        pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+        business_date = df["business_date"].iloc[0]
 
-    pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
-    business_date = df["business_date"].iloc[0]
-    pg_hook.run(f'DELETE FROM airflow_data."{staging_table}" WHERE business_date = %s;', parameters=(business_date,))
-    
-    conn = pg_hook.get_conn()
-    cur = conn.cursor()
-    cur.copy_expert(
-        f"""COPY airflow_data."{staging_table}" ({', '.join([f'"{c}"' for c in columns])}) FROM STDIN WITH CSV HEADER""",
-        file=buffer
-    )
-    conn.commit()
-    cur.close()
-    conn.close()
+        pg_hook.run(
+            f'DELETE FROM airflow_data."{staging_table}" WHERE business_date = %s;',
+            parameters=(business_date,)
+        )
 
-    return {"staging_table": staging_table, "entity": entity, "skipped": False}
+        conn = pg_hook.get_conn()
+        cur = conn.cursor()
+        cur.copy_expert(
+            f"""COPY airflow_data."{staging_table}" ({', '.join([f'"{c}"' for c in columns])}) FROM STDIN WITH CSV HEADER""",
+            file=buffer
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return {
+            "entity": entity,
+            "staging_table": staging_table,
+            "skipped": False,
+            "error": None,
+        }
+
+    except Exception as e:
+        logger.exception(f"[STAGING] Error loading to staging for {entity}: {str(e)}")
+        return {
+            "entity": entity,
+            "staging_table": None,
+            "skipped": True,
+            "error": str(e)
+        }
 
 @task
 def merge_to_production(staging_info: dict):
-    if staging_info["skipped"]:
-        return
+    entity = staging_info.get("entity")
+    
+    if staging_info.get("skipped"):
+        logger.exception(f"[MERGE] Skipping merge for {entity} – reason: {staging_info.get('error', 'Unknown')}")
+        return {
+            "entity": entity,
+            "staging_table": staging_info.get("staging_table"),
+            "skipped": True,
+            "error": staging_info.get("error", "Skipped upstream")
+        }
 
-    entity = staging_info["entity"]
-    config = ENTITY_CONFIG[entity]
-    prod_table = config["production_table"]
-    staging_table = config["staging_table"]
-    columns = config["columns"]
+    try:
+        config = ENTITY_CONFIG[entity]
+        prod_table = config["production_table"]
+        staging_table = config["staging_table"]
+        columns = config["columns"]
 
-    col_str = ", ".join([f'"{col}"' for col in columns])
-    set_clause = ",\n".join([f'"{col}" = EXCLUDED."{col}"' for col in columns])
+        col_str = ", ".join([f'"{col}"' for col in columns])
+        set_clause = ",\n".join([f'"{col}" = EXCLUDED."{col}"' for col in columns])
 
-    if entity == "poeb-rbb":
-        sql = f"""
-        INSERT INTO airflow_data."{prod_table}" ({col_str})
-        SELECT {col_str}
-        FROM airflow_data."{staging_table}";
-        """
-    else:
-        conflict_cols = '"business_date", "dtime"'
-        sql = f"""
-        INSERT INTO airflow_data."{prod_table}" ({col_str})
-        SELECT {col_str}
-        FROM airflow_data."{staging_table}"
-        ON CONFLICT ({conflict_cols}) DO UPDATE SET
-        {set_clause},
-        processed_at = CURRENT_TIMESTAMP;
-        """
-    PostgresHook(postgres_conn_id=POSTGRES_CONN_ID).run(sql)
+        #if entity == "poeb-rbb" or entity == "popmb-rmb":]
+        if entity in ["poeb-rbn", "poeb-rbb", "pomb-rbn", "popmb-rmb"]:
+            sql = f"""
+            INSERT INTO airflow_data."{prod_table}" ({col_str})
+            SELECT {col_str}
+            FROM airflow_data."{staging_table}";
+            """
+        else:
+            conflict_cols = '"business_date", "dtime"'
+            sql = f"""
+            INSERT INTO airflow_data."{prod_table}" ({col_str})
+            SELECT {col_str}
+            FROM airflow_data."{staging_table}"
+            ON CONFLICT ({conflict_cols}) DO UPDATE SET
+            {set_clause},
+            processed_at = CURRENT_TIMESTAMP;
+            """
+        PostgresHook(postgres_conn_id=POSTGRES_CONN_ID).run(sql)
+
+        return {
+            "entity": entity,
+            "staging_table": staging_table,
+            "skipped": False,
+            "error": None,
+        }
+
+    except Exception as e:
+        logger.exception(f"Error merge to production for {entity}: {str(e)}")
+        return {
+            "entity": entity,
+            "staging_table": staging_info.get("staging_table"),
+            "skipped": True,
+            "error": str(e)
+        }
 
 @task
 def cleanup_staging(staging_info: dict):
-    if staging_info["skipped"]:
-        return
-    table = staging_info["staging_table"]
-    PostgresHook(postgres_conn_id=POSTGRES_CONN_ID).run(f'TRUNCATE airflow_data."{table}";')
+    entity = staging_info.get("entity")
+    staging_table = staging_info.get("staging_table")
+
+    if staging_info.get("skipped"):
+        return {
+            "entity": entity,
+            "staging_table": staging_table,
+            "skipped": True,
+            "error": staging_info.get("error", "Skipped upstream")
+        }
+
+    try:
+        PostgresHook(postgres_conn_id=POSTGRES_CONN_ID).run(f'TRUNCATE airflow_data."{staging_table}";')
+        return {
+            "entity": entity,
+            "staging_table": staging_table,
+            "skipped": False,
+            "error": None,
+        }
+
+    except Exception as e:
+        logger.exception(f"[STAGING] Error cleanup_staging for {entity}: {str(e)}")
+        return {
+            "entity": entity,
+            "staging_table": staging_table,
+            "skipped": True,
+            "error": str(e)
+        }
 
 @task
 def log_etl_result(staging_info: dict, execution_date=None):
-    entity = staging_info["entity"]
-    pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
-    date_str = execution_date.strftime("%Y-%m-%d")
+    try:
+        entity = staging_info.get("entity", "unknown")
+        pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+        logger.exception(f"[STAGING] Error log etl result for {entity}: {staging_info.get("error")}")
 
-    if staging_info["skipped"]:
-        message = staging_info.get("error", "No data fetched or staged")
-        
-        if message.startswith("Already fetched successfully"):
-            result = "success"
+        if not execution_date:
+            raise ValueError("Missing execution_date")
+
+        date_str = execution_date.strftime("%Y-%m-%d")
+
+        if staging_info.get("skipped"):
+            message = staging_info.get("error", "No data fetched or staged")
+            #message = staging_info.get("error") or "No data fetched or staged"
+            result = "success" if message.startswith("Already fetched successfully") else "fail"
         else:
-            result = "fail"
+            try:
+                prod_table = ENTITY_CONFIG[entity]["production_table"]
+                sql = f'SELECT COUNT(*) FROM airflow_data."{prod_table}" WHERE business_date = %s;'
+                count_row = pg_hook.get_first(sql, parameters=(date_str,))
+                count = count_row[0] if count_row else 0
+                if count > 0:
+                    result = "success"
+                    message = "Loaded"
+                else:
+                    result = "fail"
+                    message = staging_info.get("error", "No data after merge1")
+            except Exception as e:
+                logger.exception(f"[STAGING] Error log etl result for {entity}: {str(e)}")
+                result = "fail"
+                message = f"Check error: {str(e)}"
 
-    else:
-        try:
-            prod_table = ENTITY_CONFIG[entity]["production_table"]
-            sql = f'SELECT COUNT(*) FROM airflow_data."{prod_table}" WHERE business_date = %s;'
-            count = pg_hook.get_first(sql, parameters=(date_str,))[0]
-            result = "success" if count > 0 else "fail"
-            message = "Loaded" if count > 0 else "No data after merge"
-        except Exception as e:
-            result = "fail"
-            message = f"Check error: {str(e)}"
+        log_sql_upsert = """
+            INSERT INTO airflow_data.pse_api_log (entity, business_date, message, result)
+            VALUES (%s, %s, %s, %s)
+            ON CONFLICT (entity, business_date)
+            DO UPDATE SET
+                message = EXCLUDED.message,
+                result = EXCLUDED.result,
+                logged_at = CURRENT_TIMESTAMP;
+        """
+        pg_hook.run(log_sql_upsert, parameters=(entity, date_str, message, result))
 
-    log_sql_upsert = """
-        INSERT INTO airflow_data.pse_api_log (entity, business_date, message, result)
-        VALUES (%s, %s, %s, %s)
-        ON CONFLICT (entity, business_date)
-        DO UPDATE SET
-            message = EXCLUDED.message,
-            result = EXCLUDED.result,
-            logged_at = CURRENT_TIMESTAMP;
-    """
-    pg_hook.run(log_sql_upsert, parameters=(entity, date_str, message, result))
+    except Exception as e:
+        logging.error(f"FAILED to log ETL result for entity '{staging_info.get('entity')}' – {str(e)}")
 
 
 @dag(
@@ -285,7 +371,7 @@ def pse_etl_main_expand():
     merged = merge_to_production.expand(staging_info=staged)
     cleanup = cleanup_staging.expand(staging_info=staged)
     cleanup.set_upstream(merged)
-    log_result = log_etl_result.expand(staging_info=staged)
+    log_result = log_etl_result.expand(staging_info=merged)
     log_result.set_upstream(merged)
 
     for task in [tables, fetched_data, staged, merged, cleanup, log_result]:
--- tasks/pse_dag_config.py
+++ tasks/pse_dag_config.py
@@ -4,7 +4,7 @@ POSTGRES_CONN_ID = "postgres_azure_vm"
 HTTP_CONN_ID = "PSE_API"
 HISTORICAL_START_DATE = datetime(2025, 6, 1)
 
-ENTITY_CONFIG = {
+ENTITY_CONFIG_all = {
     "pomb-rbn": {
         "columns": [
             "ofc", "dtime", "period", "pofmax", "pofmin", "plofmax", "plofmin",
@@ -125,23 +125,29 @@ ENTITY_CONFIG = {
     }
 }
 
-ENTITY_CONFIG_2 = {
-    "poeb-rbb": {
+ENTITY_CONFIG = {
+    "eb-rozl": {
         "columns": [
-            "ofp", "ofcd", "ofcg", "dtime", "period",
-            "dtime_utc", "period_utc", "business_date",
-            "publication_ts", "publication_ts_utc"
-        ],
-        "staging_table": "stg_poeb_rbb",
-        "production_table": "poeb_rbb"
-    },
-    "zmb": {
-        "columns": [
-            "dtime", "period", "zmb_rrd", "zmb_rrg", "zmb_fcrd", "zmb_fcrg", "zmb_frrd",
-            "zmb_frrg", "dtime_utc", "zmb_afrrd", "zmb_afrrg", "period_utc",
+            "dtime", "period", "eb_d_pp", "eb_w_pp", "dtime_utc", "period_utc",
             "business_date", "publication_ts", "publication_ts_utc"
         ],
-        "staging_table": "stg_zmb",
-        "production_table": "zmb"
+        "staging_table": "stg_eb_rozl",
+        "production_table": "eb_rozl"
+    },
+    "cmbu-tu": {
+        "columns": [
+            "dtime", "fcr_d", "fcr_g", "afrr_d", "afrr_g", "period", "mfrrd_d", "mfrrd_g",
+            "dtime_utc", "period_utc", "business_date", "publication_ts", "publication_ts_utc"
+        ],
+        "staging_table": "stg_cmbu_tu",
+        "production_table": "cmbu_tu"
+    },
+    "popmb-rmb": {
+        "columns": [
+            "com", "pom", "comrr", "dtime", "onmbp", "dtime_utc", "reserve_type",
+            "business_date", "publication_ts", "publication_ts_utc"
+        ],
+        "staging_table": "stg_popmb_rmb",
+        "production_table": "popmb_rmb"
     }
 }
